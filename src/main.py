594f3320c8cfe8dc130ce571ce191f4e8b4fc260
Joan <j.pastor1591@gmail.com>
2025-04-06 18:00:48 +0200

    Testing main design

--- src/main.py
+++ src/main.py
@@ -1,26 +1,57 @@
 import flet as ft
+from interface.controls.appbar import AppBar
+from shared.themes import EmeraldTheme
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    # Window settings
+    page.window.maximizable = False
+    page.window.resizable = False
+    page.window.width = 980
+    page.window.height = 720
+    page.title = "Download Manager"
 
-    def increment_click(_):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    # Main page design
+    page.fonts = {
+        "MontserratR": r"fonts\Montserrat-Regular.ttf",
+    }
+    page.theme = EmeraldTheme("MontserratR")
+    page.theme_mode = ft.ThemeMode.DARK
+    page.padding = 8
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+    def pick_files_result(e: ft.FilePickerResultEvent):
+        print(type(e.path), e.path)
+        selected_files.value = (
+            e.path
+        )
+        selected_files.update()
+
+    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
+    selected_files = ft.TextField()
+
+    page.overlay.append(pick_files_dialog)
+
     page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
+        ft.Container(
+            image=ft.DecorationImage(src="bg01.png", fit=ft.ImageFit.COVER),
             expand=True,
+            padding=32,
+            content=ft.Column(
+                controls=[
+                    ft.ElevatedButton(
+                        "Styled button 1",
+                        style=ft.ButtonStyle(
+                            shape=ft.RoundedRectangleBorder(2),
+
+                        ),
+                        on_click=lambda _: pick_files_dialog.get_directory_path()
+                    ),
+                    selected_files
+                ]
+            )
         )
     )
 
 
-ft.app(main)
+if __name__ == '__main__':
+    ft.app(main)
