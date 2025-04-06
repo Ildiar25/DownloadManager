e58e09a506d8ae86ca6f3b9348be61a48637af09
Joan <j.pastor1591@gmail.com>
2025-04-06 19:47:29 +0200

    Implemented move files methods

---
+++ src/features/folder_organizer.py
@@ -0,0 +1,81 @@
+import os
+from pathlib import Path
+
+from src.features.models.file import MyFile
+from src.features.path_repository import PathRepository
+
+
+class FolderOrganizer:
+	def __init__(self, main_path: Path, ext_white_dict: dict[str, list[str]], ext_black_list: list[str]) -> None:
+		self.file_manager = PathRepository(main_path)
+		self.ext_white_list = ext_white_dict
+		self.ext_black_list = ext_black_list
+		self.counter = 0
+
+	def organize_folder(self) -> None:
+		if os.listdir(self.file_manager.main_folder.path):
+			files = self.__prepare_main_folder(self.file_manager.main_folder.path)
+			temp_folder = self.file_manager.create_directory("temp")
+
+			for file in files:
+				self.file_manager.move_item(file, temp_folder)
+
+		files = self.file_manager.list_folder_items()
+
+		for file in files:
+			if file.extension in self.ext_white_list["images"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Imágenes"))
+				self.counter += 1
+
+			if file.extension in self.ext_white_list["audios"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Audios"))
+				self.counter += 1
+
+			if file.extension in self.ext_white_list["videos"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Vídeos"))
+				self.counter += 1
+
+			if file.extension in self.ext_white_list["documents"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Documentos"))
+				self.counter += 1
+
+			if file.extension in self.ext_white_list["compress"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Carpetas comprimidas"))
+				self.counter += 1
+
+			if file.extension in self.ext_white_list["executables"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Ejecutables"))
+				self.counter += 1
+
+			if file.extension in self.ext_white_list["web"]:
+				self.file_manager.move_item(file, self.file_manager.create_directory("Internet"))
+				self.counter += 1
+
+	def __prepare_main_folder(self, main_path: Path) -> list[MyFile]:
+		folder_files = []
+
+		def _get_through_directories(actual_path: Path):
+			root, subfolders, files = next(os.walk(actual_path))
+			try:
+				for file in files:
+					name, extension = file.rsplit(".", 1)
+					folder_files.append(self.file_manager.get_file(name, extension, Path(root).joinpath(file)))
+
+				for folder in subfolders:
+					_get_through_directories(Path(root).joinpath(folder))
+
+			except OSError as path_error:
+				print(f"There's {path_error} error while trying to access the next path: {root}")
+				return
+
+		_get_through_directories(main_path)
+		return folder_files
+
+
+def main():
+	organizer = FolderOrganizer(Path(r"C:\Users\jpast\Desktop\test_downloads"), {}, [])
+	organizer.organize_folder()
+
+
+if __name__ == '__main__':
+	main()
---
+++ src/features/path_repository.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+
+from .models.path import MyPath
+from .models.file import MyFile
+
+
+class PathRepository:
+	def __init__(self, main_folder: Path) -> None:
+		self.main_folder = MyPath(main_folder)
+		self.folder_items = self.list_folder_items()
+
+	def create_directory(self, dir_name) -> Path:
+		new_folder = self.main_folder.joinpath(dir_name)
+
+		if not os.path.exists(new_folder):
+			os.makedirs(new_folder, exist_ok=True)
+
+		return new_folder
+
+	def list_folder_items(self) -> list[MyFile]:
+		folder_items = []
+		files = [file for file in os.listdir(self.main_folder.path) if os.path.isfile(self.main_folder.joinpath(file))]
+
+		for file in files:
+			name, extension = file.rsplit(".", 1)
+			folder_items.append(MyFile(name, extension, self.main_folder.joinpath(file)))
+
+		return folder_items
+
+	@staticmethod
+	def move_item(file: MyFile, new_path: Path) -> None:
+		os.replace(file.path, new_path.joinpath(file.complete_name()))
+
+	@staticmethod
+	def get_file(name: str, extension: str, path: Path) -> MyFile:
+		return MyFile(name, extension, path)
