c1b24bf69d3fae4549e6c5d00440ec0cdaa001b2
Joan <j.pastor1591@gmail.com>
2025-04-12 08:33:28 +0200

    Main file update

---
+++ src/interface/controls/textfield.py
@@ -0,0 +1,29 @@
+import flet as ft
+
+
+class CustomTextField(ft.TextField):
+	"""Creates a custom text field."""
+	def __init__(self, error: str | None = None, **kwargs) -> None:
+		super().__init__(**kwargs)
+
+		# Specific settings
+		self.error_text = error
+
+		# Text design settings
+		# self.cursor_color = primaryCorporateColor
+		# self.selection_color = secondaryTextColor
+		# self.label_style = ft.TextStyle(color=secondaryTextColor)
+		# self.hint_style = ft.TextStyle(color=secondaryTextColor)
+
+		# Field design settings
+		self.border_radius = 4
+		# self.border_color = neutral20
+		# self.focused_border_color = primaryCorporateColor
+
+	def reset_error(self) -> None:
+		self.error_text = None
+		self.update()
+
+	def show_error(self, msg: str) -> None:
+		self.error_text = msg
+		self.update()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import flet as ft
-from interface.controls.appbar import AppBar
+from interface.dashboard import Dashboard
 
 
 def main(page: ft.Page):
@@ -8,6 +8,7 @@ def main(page: ft.Page):
     page.window.resizable = False
     page.window.width = 980
     page.window.height = 720
+    page.padding = 0
     page.title = "Download Manager"
 
     # Main page design
@@ -16,40 +17,8 @@ def main(page: ft.Page):
     }
     page.theme = ft.Theme(font_family="MontserratR", color_scheme_seed=ft.Colors.TEAL)
     page.theme_mode = ft.ThemeMode.DARK
-    page.padding = 8
 
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        print(type(e.path), e.path)
-        selected_files.value = (
-            e.path
-        )
-        selected_files.update()
-
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-    selected_files = ft.TextField()
-
-    page.overlay.append(pick_files_dialog)
-
-    page.add(
-        ft.Container(
-            image=ft.DecorationImage(src="bg01.png", fit=ft.ImageFit.COVER),
-            expand=True,
-            padding=32,
-            content=ft.Column(
-                controls=[
-                    ft.ElevatedButton(
-                        "Styled button 1",
-                        style=ft.ButtonStyle(
-                            shape=ft.RoundedRectangleBorder(2),
-
-                        ),
-                        on_click=lambda _: pick_files_dialog.get_directory_path()
-                    ),
-                    selected_files
-                ]
-            )
-        )
-    )
+    page.add(Dashboard(page))
 
 
 if __name__ == '__main__':
